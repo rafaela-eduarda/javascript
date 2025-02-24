aaa3559450dc27c7f4a666606291cf7f4a3eba22
rafaela-eduarda <149601165+rafaela-eduarda@users.noreply.github.com>
2025-02-24 19:28:09 +0000

    3

---
+++ concatenacao.js
@@ -0,0 +1,9 @@
+let salaJS = ["Kaique", "Gabriel", "Allan"];
+let salaPython = ["Lucas", "Larissa", "Maylon"];
+
+salaPython.pop("Maylon");
+salaPython.push("sara");
+
+let salaUnificada = salaJS.concat(salaPython);
+
+console.log(salaUnificada);
