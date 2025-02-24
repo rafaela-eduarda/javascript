affae5ef211e14fb96edf195c9c6df26ee32fc6a
rafaela-eduarda <149601165+rafaela-eduarda@users.noreply.github.com>
2025-02-24 19:37:29 +0000

    333

---
+++ dividirVetor.js
@@ -0,0 +1,16 @@
+let alunos = [
+    "Gabriel",
+    "Kaique",
+    "Lucas",
+    "Allan",
+    "matheus",
+    "Sara",
+    "Rafaela"
+    "Kelly"
+    "Maylon"
+];
+
+let sala1 = alunos.slice(0, alunos.length / 2);
+let sala2 = alunos.slice(alunos.length / 2);
+console.log(sala1);
+console.log(sala2);
